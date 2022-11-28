80236e592c013eeb8dc7a9f26f66698b336471c5
mhmdfakhrim <muhfakhri4@gmail.com>
2022-11-28 16:25:05 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackUsingSingleLinkList
+{
+    class node
+    {
+        public int info;
+        public node next;
+        public node(int i, node n)
+        {
+            info = i;
+            next = n;
+        }
+    }
+    class Stack
+    {
+        node top;
+        public Stack()
+        {
+            top = null;
+        }
+    }
+    bool empty()
+    {
+        if (top == null)
+            return (true);
+
+    }
+        static void Main(string[] args)
+        {
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public void display()
+{
+     node top;
+
+}
