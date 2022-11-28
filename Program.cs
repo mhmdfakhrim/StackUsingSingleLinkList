827e1868b44daabb11d4f42de9283e398acd9fa4
mhmdfakhrim <muhfakhri4@gmail.com>
2022-11-28 16:57:11 +0700

    latihan

--- Program.cs
+++ Program.cs
@@ -23,54 +23,89 @@ namespace StackUsingSingleLinkList
         {
             top = null;
         }
-    }
-    bool empty()
-    {
-        if (top == null)
-            return (true);
-        else 
-            return (false);
-    }
-
-    public void push(int element)
-    {
-        node fresh;
-        fresh = new node(element, null);
-
-        fresh.next = top;
-        top = fresh;
-        Console.WriteLine("\n" + element + "pushed");
-    }
-    
-    public void pop()
-    {
-        Console.WriteLine("\n The poped element is: " + top.info);
-        top = top.next; // make top point to the next is squence
-    }
-
-    public void display()
-    {
-        node tmp;
-        if (empty())
-            Console.WriteLine("\nStackEmpty");
-        else
+        bool empty()
         {
-            for (tmp = top; tmp != null; tmp = tmp.next)
+            if (top == null)
+                return (true);
+            else
+                return (false);
+        }
+
+        public void push(int element)
+        {
+            node fresh;
+            fresh = new node(element, null);
+
+            fresh.next = top;
+            top = fresh;
+            Console.WriteLine("\n" + element + "pushed");
+        }
+
+        public void pop()
+        {
+            Console.WriteLine("\n The poped element is: " + top.info);
+            top = top.next; // make top point to the next is squence
+        }
+
+        public void display()
+        {
+            node tmp;
+            if (empty())
+                Console.WriteLine("\nStackEmpty");
+            else
             {
-                Console.WriteLine(tmp.info);
+                for (tmp = top; tmp != null; tmp = tmp.next)
+                {
+                    Console.WriteLine(tmp.info);
+                }
             }
         }
     }
+   class program
+    {
         static void Main(string[] args)
         {
-        Stack s = new Stack();
-        while(true)
-        {
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine();
+            Stack s = new Stack();
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("\n***Stack Menu***\n");
+                Console.WriteLine("1. Push ");
+                Console.WriteLine("2. Pop ");
+                Console.WriteLine("3. Display ");
+                Console.WriteLine("4. Exit ");
+                Console.Write("\nEnter your choice: ");
+                string Input = Console.ReadLine();
+                char ch = Convert.ToChar(Input == "" ? "0" : Input);
+                switch (ch)
+                {
+                    case '1':
+                        Console.Write("\nEnter a number :");
+                        int num = Convert.ToInt32(Console.ReadLine());
+                        s.push(num);
+                        break;
+                    case '2':
+                        if (s.empty())
+                        {
+                            Console.WriteLine("\nStack Empty");
+                            break;
+                        }
+                        s.pop();
+                        break;
+                    case '3':
+                        s.display();
+                        break;
+                    case '4':
+                        return;
+                    default:
+                        Console.WriteLine("\nInvalid Choice");
+
+                }
+            }
         }
-        }
+    }
+
+       
     }
 
 
